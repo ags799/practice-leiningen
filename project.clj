230a30a8f85716bf91c24e30162f8a974b62f611
Andrew Sharp <andrew.sharp57@gmail.com>
2017-02-25 22:29:53 -0500

    replace FIXMEs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject practice-leiningen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "For practicing Leiningen"
+  :url "https://github.com/ags799/practice-leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
